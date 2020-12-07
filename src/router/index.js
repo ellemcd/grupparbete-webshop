16dbd852a5ce0f5f9f63b6827cc892a59255bd20
jacob <jacobnilsson98@hotmail.com>
2020-12-07 15:58:42 +0100

    Added router-link

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import Checkout from '../views/Checkout.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,18 @@ const routes = [
     component: Home
   },
   {
+    path: '/checkout',
+    name: 'Checkout',
+    component: Checkout
+  },
+  {
     path: '/about',
     name: 'About',
+    component: About,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    
   }
 ]
 
