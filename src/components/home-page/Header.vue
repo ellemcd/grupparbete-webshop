c3ec91a0966a55958d0fd6ed60f0a0a3985c54c8
jacob <jacobnilsson98@hotmail.com>
2020-12-08 12:02:45 +0100

    test header

--- src/components/home-page/Header.vue
+++ src/components/home-page/Header.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1>Header picture </h1>
+  <div class="header">
+  <img src="https://images.pexels.com/photos/1884581/pexels-photo-1884581.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260">
+  </div>
 </template>
 
 <script>
@@ -9,5 +11,8 @@ export default {
 </script>
 
 <style scope>
-
+.header {
+  height: 100vh;
+  width: 100vh;
+}
 </style>
